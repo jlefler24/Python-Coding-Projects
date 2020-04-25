98010baa27d60b9df4c09d2c917b77b71946e02c
jlefler24 <lefler24@aol.com>
2020-04-24 19:47:42 -0600

    sql_drill commit
    sql python drill

--- sql_drill.py
+++ sql_drill.py
@@ -1,1 +1,28 @@
+import sqlite3
+
+conn = sqlite3.connect('drill_1.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_fList\
+        (ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_fileN TEXT \
+        )")
+    conn.commit()
+
+
+
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+with conn:
+    for file in fileList:
+        if file.endswith('.txt'):
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_fList(col_fileN) VALUES (?)", \
+                (file,))
+            print (file)
+            
+
+    
+
 
